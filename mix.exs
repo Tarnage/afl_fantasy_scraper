8d6781817f0293d16878cc5c5b53a31e22d56600
Tom <70882644+Tarnage@users.noreply.github.com>
2023-05-10 16:43:07 +0800

    updated deps

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule AflFantasyScraper.MixProject do
   def project do
     [
       app: :afl_fantasy_scraper,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "AFL Scraper",
+      source_url: "https://github.com/Tarnage/afl_fantasy_scraper"
     ]
   end
 
@@ -27,9 +31,26 @@ defmodule AflFantasyScraper.MixProject do
   defp deps do
     [
       {:floki, "~> 0.34.2"},
-      {:req, "~> 0.3"}
+      {:req, "~> 0.3"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "This is a project that will scrape AFL data from footywire"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "postgrex",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/Tarnage/afl_fantasy_scraper"}
     ]
   end
 end
